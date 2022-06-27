17040203d2c4df7d0887beefb49cb1537a01952e
AgataBaranska <baranskagata@gmail.com>
2022-06-27 19:09:17 +0200

    added cart modal

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {useState} from 'react'
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
-
+import Cart from './components/Cart/Cart'
 
 function App() {
 
@@ -17,6 +17,7 @@ function App() {
 
   return (
     <CartProvider>
+      <Cart/>
       {cartIsShown && <div>Cart ... </div>}
       <Header onShowCart={showCartHandler} />
       <main>
